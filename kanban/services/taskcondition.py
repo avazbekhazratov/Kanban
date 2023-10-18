16d121d137c62c9ede7332ee74386459c596870b
zeki704 <134948807+zeki704@users.noreply.github.com>
2023-10-18 19:34:28 +0500

    TaskCondition

--- kanban/services/taskcondition.py
+++ kanban/services/taskcondition.py
@@ -2,7 +2,7 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from kanban.serializers import TaskConditionSerializer
-from kanban.models.core import Board, TaskCondition, TaskItem, SubTask, BoardMember
+from kanban.models.core import TaskCondition, TaskItem, SubTask, BoardMember
 
 
 class TaskCondt(GenericAPIView):
@@ -24,10 +24,16 @@ class TaskCondt(GenericAPIView):
 
     def post(self, request):
         data = request.data
-        if "title" not in data or not data['title']:
+        if "board" not in data or not data['board']:
             return Response({"error": "empty"})
 
         if "creator" not in data or not data['creator']:
+            return Response({"error": "empty"})
+
+        if "title" not in data or not data['title']:
+            return Response({"error": "empty"})
+
+        if "desc" not in data or not data['desc']:
             return Response({"error": "empty"})
 
         serializer = self.get_serializer(data=data)
@@ -37,8 +43,19 @@ class TaskCondt(GenericAPIView):
 
     def put(self, request, pk):
         data = request.data
+
+        if "board" not in data or not data['board']:
+            return Response({"error": "empty"})
+
+        if "creator" not in data or not data['creator']:
+            return Response({"error": "empty"})
+
         if "title" not in data or not data['title']:
             return Response({"error": "empty"})
+
+        if "desc" not in data or not data['desc']:
+            return Response({"error": "empty"})
+
         bosa = TaskCondition.objects.filter(pk=pk).first()
         if bosa:
             serializer = self.get_serializer(data=data, instance=bosa)
--- kanban/urls.py
+++ kanban/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import Items
+from .services import TaskCondt
 
 urlpatterns = [
     path('item/', Items.as_view()),
     path("item/<int:pk>/", Items.as_view()),
+
+    path('item/', TaskCondt.as_view()),
+    path("item/<int:pk>/", TaskCondt.as_view()),
 ]

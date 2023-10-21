9a6741e5fbd6130007cbea93bf73d745910ca2f3
zeki704 <134948807+zeki704@users.noreply.github.com>
2023-10-21 17:42:43 +0500

    Merge branch 'main' of C:\Users\user\Desktop\Kanban with conflicts.

--- kanban/services/task_item.py
+++ kanban/services/task_item.py
@@ -1,60 +1,10 @@
-from rest_framework.response import Response
-from rest_framework.generics import GenericAPIView
-from kanban.models.core import TaskItem
-from kanban.serializers import TaskItemSerializer
-from rest_framework import status
+import unittest
 
 
-class TaskItemView(GenericAPIView):
-    serializer_class = TaskItemSerializer
-
-    def get(self, request, pk=None):
-        if pk:
-            task = TaskItem.objects.filter(id=pk).first()
-            if task:
-                serializer = self.get_serializer(task).data
-                return Response({"succes": serializer}, status=status.HTTP_200_OK)
-            return Response({"Error": "Not Found"}, status=status.HTTP_404_NOT_FOUND)
-        taskMem = TaskItem.objects.all()
-        serializer = self.get_serializer(taskMem, many=True).data
-        return Response({'Success': serializer}, status=status.HTTP_200_OK)
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
 
 
-
-    def post(self, request):
-        data = request.data
-        if "title" not in data or not data['title']:
-            return Response({"Error": "Fields are empty"}, status=status.HTTP_404_NOT_FOUND)
-        if "creator" not in data or not data['creator']:
-            return Response({"Error": "Fields are empty"}, status=status.HTTP_404_NOT_FOUND)
-        if "task_condition" not in data or not data['task_condition']:
-            return Response({"Error": "Fields are empty"}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'Success': "you have added"})
-    def put(self, request, pk):
-        data = request.data
-        if "title" not in data or not data['title']:
-            return Response({"error": "empty"})
-        if "creator" not in data or not data['creator']:
-            return Response({"Error": "Fields are empty"}, status=status.HTTP_404_NOT_FOUND)
-        if "task_condition" not in data or not data['task_condition']:
-            return Response({"Error": "Fields are empty"}, status=status.HTTP_404_NOT_FOUND)
-
-        BoardMem = TaskItem.objects.filter(pk=pk).first()
-        if BoardMem:
-            serializer = self.get_serializer(data=data, instance=BoardMem)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response({'Success': "you have edited"}, status=status.HTTP_200_OK)
-        return Response({'Error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
-
-    def delete(self, request, pk):
-        BoardMem = TaskItem.objects.filter(id=pk).first()
-        if BoardMem:
-            BoardMem.delete()
-            return Response({"Success": "Successfully deleted"}, status=status.HTTP_200_OK)
-        return Response({"Error": "Data not found"}, status=status.HTTP_404_NOT_FOUND)
-
+if __name__ == '__main__':
+    unittest.main()

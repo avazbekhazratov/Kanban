b3aaff0368a45497d4cfdb4d925abca0234fb44f
Akbarali <tulkinovali@gmail.com>
2023-11-02 15:33:02 +0500

    latest

--- kanban/models/core.py
+++ kanban/models/core.py
@@ -7,7 +7,6 @@ class Board(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
 
-
     def __str__(self):
         return f"{self.title}"
 

d1d7ebb860632e2cd9f9d7a84cd18772e1a251af
Akbarali <tulkinovali@gmail.com>
2023-11-02 15:50:14 +0500

    mini push

--- src/settings.py
+++ src/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3wh=v2_frf5+sokdq$qjimd6ke&@*dyfh4rdj4&w)2&eyiq!pj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', '192.168.42.197']
+ALLOWED_HOSTS = ['138.68.109.195']
 
 
 # Application definition
@@ -83,8 +83,12 @@ AUTH_USER_MODEL = "kanban.User"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Kanban',
+        'USER': 'Kanbanuser',
+        'PASSWORD': 'FinTechHub',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -107,10 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://192.168.42.141:3000"
-]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -129,7 +130,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / 'static_files']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
